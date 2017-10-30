8d6b95495b3bc7178b4442c5e64787ae76bff884
Pham <Daniel.Pham@stlcop.local>
2017-10-30 15:06:22 -0500

    Number # 2 Complete
    The code for Number # 2

--- Number2.R
+++ Number2.R
@@ -11,5 +11,15 @@ size_mr_data <- data.frame(
     148.949, 11.966, 46.414, 123.287, 106.663, 20.619, 180.150, 
     200.830, 224.779, 148.940, 112.430, 286.847, 46.347, 142.863, 
     106.670, 119.660, 104.150, 33.165, 4.900, 4.865))
+
+plot(size_mr_data$body_mass , size_mr_data$metabolic_rate , xlab = "Body Mass" , ylab = "Metabolic Rate")
+
+log_body_mass <- log(size_mr_data$body_mass)
+log_metabolic_rate <- log(size_mr_data$metabolic_rate)
     
+plot(log(size_mr_data$body_mass) , log(size_mr_data$metabolic_rate) , xlab = "Log Body Mass" , ylab = "Log Metabolic Rate")
+
+plot(size_mr_data$body_mass , size_mr_data$metabolic_rate , xlab = "Body Mass" , ylab = "Metabolic Rate" , log = "xy" )   
+
 #code goes here
+
